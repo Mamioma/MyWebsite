0642a2db08efd142e0795d9245285493617de362
Mamioma <mamiom7ma@gmail.com>
2023-05-28 14:24:09 -0700

    test can I able to track user's location based on their ip address

--- script.js
+++ script.js
@@ -1,5 +1,3 @@
-let contentLoad = 0;
-
 document.addEventListener("DOMContentLoaded", function () {
     const menuItems = document.querySelectorAll(".link");
     const submenuItems = document.querySelectorAll(".sublink");
@@ -90,27 +88,4 @@ document.addEventListener("DOMContentLoaded", function () {
             enterCount--;
         }
     });
-
-    if (contentLoad == 0)
-    {
-        contentLoad += 1
-        fetch('https://api.ipgeolocation.io/ipgeo?apiKey=e0b306fb6ba74ec5b98a668ec24fd667')
-            .then(response => response.json())
-            .then(data => {
-                console.log('Your location is ' + data.city + ', ' + data.state_prov + ', ' + data.country_name);
-                // Here you can handle the location data and display different content based on it
-                // For example:
-                const country = data.country_name;
-
-                if (country === 'United States') {
-                    window.location.href = '/US.html';
-                } else if (country === 'China') {
-                    window.location.href = '/China.html';
-                } else {
-                    window.location.href = "/Nothing.html";
-                }
-            })
-            .catch(console.error);
-    }
-
 });
